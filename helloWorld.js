b6e59a42a6dbd8b5780382bab116d4a65239a31f
Murielle Brugoux <muriellebrugoux@yahoo.fr>
2023-02-19 17:32:54 +0000

    Changed the script to request user's name and printing it out

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,4 @@
-console.log("Git is Awesome!")
+// changing initial script (console.log("Git is Awesome!") to a new script requesting an input from the user
+// and printing it out;
+userName = prompt("Please enter your name: ")
+console.log(`Welcome, ${userName}!`)
